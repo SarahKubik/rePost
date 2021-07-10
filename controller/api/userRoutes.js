c4115774d364253c63fcc353b09031650a8f1eb7
Curtisaurus <ccdrullinger@gmail.com>
2021-07-10 14:06:43 -0400

    CRUD for User with authorization and session id check

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
   try {
@@ -55,6 +56,71 @@ router.post('/logout', (req, res) => {
     });
   } else {
     res.status(404).end();
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const usersData = await User.findAll({
+      attributes: { exclude: ['password'] }
+    });
+    
+    res.status(200).json(usersData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:name', async (req, res) => {
+  try {
+    const usersData = await User.findOne({ 
+      where: { name: req.params.name },
+      attributes: { exclude: ['password'] }
+    });
+    
+    res.status(200).json(usersData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
