5cb9039b32e9d9f0bae3cc6d216295749270a878
Drew Selden <seldend1@gmail.com>
2021-07-10 19:12:04 -0400

    login.js and login.handlebars updated

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/homepage');
       } else {
         alert(response.statusText);
       }
@@ -36,7 +36,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/homepage');
       } else {
         alert(response.statusText);
       }
