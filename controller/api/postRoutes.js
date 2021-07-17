06a61e05c21e71792c91573da074b1fd44c68825
Curtisaurus <ccdrullinger@gmail.com>
2021-07-17 13:49:10 -0400

    added delete route for posts

--- controller/api/postRoutes.js
+++ controller/api/postRoutes.js
@@ -42,5 +42,25 @@ router.get('/:user_id', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err)
   }
-})
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
