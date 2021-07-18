af012907535910000a818f6b5fcff731b23d4a75
Curtisaurus <ccdrullinger@gmail.com>
2021-07-17 22:08:00 -0400

    fixed several issues

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -44,7 +44,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       posts,
-      logged_in: true
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
